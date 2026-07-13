ea7b83306c5afb3eccb6e01ab9e9182e8a3bd725
hadiope <hadiamg@atomicmail.io>
2026-07-13 20:14:21 +0330

    Fix: Add missing Supabase import in profile_screen.dart

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../core/constants.dart';
 import '../core/greetings.dart';
